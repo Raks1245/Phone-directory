1b9be26f647f40fd1674e7ef1e9520a971328313
Rakshith N <137985879+Raks1245@users.noreply.github.com>
2023-07-08 13:49:02 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 
 class App extends Component {
 
+  clickHandler(message) {
+    alert(message);
+  }
+  
   render() {
-
     let subscribers = [
       {
         id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
+        name: "Shilpa",
+        phone: "8296380748"
       },
       {
         id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
+        name: "Rakshith",
+        phone: "9481245042"
       }
     ];
 
@@ -35,11 +38,12 @@ class App extends Component {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
-              <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn" onClick={this.clickHandler.bind(this, "Delete Clicked")}>Delete</button>
                 </span>
               </div>
             })
+            
           }
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import AddSubscriber from './AddSubscribe';
 import reportWebVitals from './reportWebVitals';
+import './common/common.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AddSubscriber />
   </React.StrictMode>
 );
 
